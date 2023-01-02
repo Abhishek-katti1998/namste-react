b8766a124e6dfa9f3242a6b27c63de340e7aa6eb
abhishek-katti <abhishek@varahaag.com>
2023-01-02 10:40:09 +0530

    week-2-tute

---
+++ app.js
@@ -0,0 +1,16 @@
+import React  from "react";
+import  ReactDOM  from "react-dom/client";
+
+// const heading=React.createElement('h1',null,"namaste");
+
+// const heading1=React.createElement('h1',{key:'1'},"heading1 from parcel");
+// const heading2=React.createElement('h1',{key:'2'},"heading2");
+// console.log(heading1);
+// //multiple children can be created by passing array of elements(3rd argument)
+// const container=React.createElement('div',{
+//     id:"container"
+// },[heading1,heading2]);
+// const root=ReactDOM.createRoot(document.getElementById('root'));
+// //passing react element
+// root.render(container);
+const head=(<h1>asmd;</h1>);
